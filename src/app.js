1c5551e88172a6e063582a0df82abfecf03447b5
anishkumar <anishreish9@gmail.com>
2024-03-03 15:30:50 +0530

    router changes

--- src/app.js
+++ src/app.js
@@ -11,4 +11,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//user routers
+import userRouter from "./routes/user.routes.js";
+
+//router decleration
+app.use("/api/v1/user", userRouter)
 export { app }
